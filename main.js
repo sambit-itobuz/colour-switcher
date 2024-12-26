96f7edc9d4b296b3e00fec117f4c160e6817c9b9
Sambit Sarkar <sambit@itobuz.com>
2024-12-26 18:06:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function colorInfo() {
+    document.body.style.backgroundColor = "aqua";
+}
+
+function colorLight() {
+    document.body.style.backgroundColor = "white";
+}
+
+function colorDark() {
+    document.body.style.backgroundColor = "black";
+}
+
+function colorSecondary() {
+    document.body.style.backgroundColor = "grey";
+}
